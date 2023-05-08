e7b7ce756486a6a0dd18fad6be4b894e8e86db54
Budaházi Bence <budahazi.bence@petrik.hu>
2023-05-08 11:25:54 +0200

    Függvények definiálása

--- DolgozatProject/Dolgozat.cs
+++ DolgozatProject/Dolgozat.cs
@@ -2,6 +2,76 @@
 {
     public class Dolgozat
     {
+        private List<int> pontok;
+        public Dolgozat()
+        {
 
+        }
+
+        public void PontFelvesz(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MindenkiMegirta
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Bukas
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Elegseges
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Kozepes
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Jo
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Jeles
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Gyanus(int kivalok)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Ervenytelen
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
